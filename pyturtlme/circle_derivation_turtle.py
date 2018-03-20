e3ce70b71d2917e3682889f74cf17ba410e46e2b
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2018-03-20 17:06:38 +0400

    circle drivation turtle version

---
+++ pyturtlme/circle_derivation_turtle.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 17:00:07 2018
+
+@author: Dell
+"""
+
+import turtle  
+
+t = turtle.Turtle()   
+screen=turtle.Screen()  
+
+for i in range(500):
+    t.fd(1)
+    t.right(1)
+              
+screen.exitonclick()
